1984d976a76fa38d0f03963b5347a7208cb882fa
edpau <edpau@me.com>
2025-06-19 15:25:52 +0100

    chore: initial project setup

---
+++ src/main/java/com/bdaytracker/backend/BdayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bdaytracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdaytracker/backend/BdayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdaytracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
